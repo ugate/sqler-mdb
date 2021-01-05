cbe8eb65012885d3ce5147149014d6e0aee34ed1
whoover <william.hoover@nemours.org>
2021-01-05 15:34:45 -0700

    [ci skip] Deployment script fixes

--- test/run.sh
+++ test/run.sh
@@ -23,17 +23,21 @@ while [ $attempt -le 79 ]; do
       [[ -z "$GITHUB_TOKEN" ]] && { echo "Missing GITHUB_TOKEN in shell. Failed to \"$3\" in docker container \"$2\"" >&2; exit 1; }
       [[ -z "$NPM_TOKEN" ]] && { echo "Missing NPM_TOKEN in shell. Failed to \"$3\" in docker container \"$2\"" >&2; exit 1; }
 
+      docker exec -it $2 bash -c '[[ -n "$GITHUB_TOKEN" ]] && { echo "GITHUB_TOKEN found"; } || echo "!!!!!!!!!!!!! GITHUB_TOKEN not found !!!!!!!!!!!!!"'
+
       CMD="npm run jsdocp-deploy"
       #docker exec -it $2 bash -c '[[ -z "$GITHUB_TOKEN" ]] && { echo "Missing GITHUB_TOKEN" >&2; exit 1; }'
       #[[ $? != 0 ]] && { echo "Failed to \"$3\" at \"$CMD\" in docker container \"$2\": Missing GITHUB_TOKEN env var in container" >&2; exit 1; }
-      docker exec -it $2 bash -c '[[ -n "$GITHUB_TOKEN" ]] && { echo "GITHUB_TOKEN found"; } || echo "GITHUB_TOKEN not found!"'
+      docker exec -it $2 bash -c "git pull"
+      [[ $? != 0 ]] && { echo "Failed to \"$3\" at \"$CMD\" for \"git pull\" in docker container \"$2\"" >&2; exit 1; }
       docker exec -it $2 bash -c "$CMD"
       [[ $? != 0 ]] && { echo "Failed to \"$3\" at \"$CMD\" in docker container \"$2\"" >&2; exit 1; }
+
+      docker exec -it $2 bash -c '[[ -n "$NPM_TOKEN" ]] && { echo "NPM_TOKEN found"; } || echo "!!!!!!!!!!!!! NPM_TOKEN not found !!!!!!!!!!!"'
 
       CMD="npm publish"
       #docker exec -it $2 bash -c '[[ -z "$NPM_TOKEN" ]] && { echo "Missing NPM_TOKEN" >&2; exit 1; }'
       #[[ $? != 0 ]] && { echo "Failed to \"$3\" at \"$CMD\" in docker container \"$2\": Missing NPM_TOKEN env var in container" >&2; exit 1; }
-      docker exec -it $2 bash -c '[[ -n "$GITHUB_TOKEN" ]] && { echo "GITHUB_TOKEN found"; } || echo "GITHUB_TOKEN not found!"'
       docker exec -it $2 bash -c 'echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc'
       [[ $? != 0 ]] && { echo "Failed to \"$3\" at \"$CMD\" in docker container \"$2\": Failed to write .npmrc" >&2; exit 1; }
       docker exec -it $2 bash -c "$CMD"
@@ -52,5 +56,3 @@ if [[ $health1 != "healthy" || $health2 != "healthy"  ]]; then
   docker logs --details $2
   exit 1
 fi
-# while [ "`docker inspect -f {{.State.Health.Status}} $1`" != "healthy" ]; do sleep 2; done
-# while [ "`docker inspect -f {{.State.Health.Status}} $2`" != "healthy" ]; do sleep 2; done
