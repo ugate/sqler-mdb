da0aa9423bcdec5ea11dab56db490415b1c298cf
whoover <william.hoover@nemours.org>
2021-02-19 11:38:42 -0700

    [FEATURE]: Transactions are now performed on an actual transaction instance rather than an SQL execution result (sqler v6 support).

--- index.js
+++ index.js
@@ -3,48 +3,6 @@
 const MAX_MDB_NAME_LGTH = 64;
 
 /**
- * MariaDB + MySQL specific extension of the {@link SQLERConnectionOptions} from the [`sqler`](https://ugate.github.io/sqler/) module.
- * @typedef {SQLERConnectionOptions} MDBConnectionOptions
- * @property {Object} driverOptions The `mariadb` (w/MySQL support) module specific options. __Both `connection` and `pool` will be merged when generating
- * the connection pool.__
- * @property {Object} [driverOptions.connection] An object that will contain properties/values that will be used to construct the MariaDB + MySQL connection
- * (e.g. `{ database: 'mydb', timezone: '-0700' }`). See the `mariadb` module documentation for a full listing of available connection options.
- * When a property value is a string surrounded by `${}`, it will be assumed to be a property that resides on either the {@link SQLERPrivateOptions}
- * passed into the {@link Manager} constructor or a property on the {@link MDBConnectionOptions} itself (in that order of precedence). For example, 
- * `connOpts.host = '127.0.0.1'` and `driverOptions.connection.host = '${host}'` would be interpolated into `driverOptions.connection.host = '127.0.0.1'`.
- * In contrast to `privOpts.username = 'someUsername' and `driverOptions.connection.user = '${username}'` would be interpolated into
- * `driverOptions.connection.user = 'someUsername'`.
- * Interpoaltions can also contain more than one reference. For example, `driverOptions.connection.someProp = '${protocol}:${host}'` for 
- * `privOpts = { protocol: 'TCP', host: 'example.com' }` would become `someProp='TCP:example.com'`.
- * @property {Object} [driverOptions.pool] The pool `conf` options that will be passed into `mariadb.createPool(conf)`. See the `mariadb` module for a full
- * listing of avialable connection pooling options.
- * __Using any of the generic `pool.someOption` will override the `conf` options set on `driverOptions.pool`__ (e.g. `pool.max = 10` would override 
- * `driverOptions.pool.connectionLimit = 20`).
- * When a value is a string surrounded by `${}`, it will be assumed to be a _constant_ property that resides on the `mariadb` module and will be interpolated
- * accordingly.
- * For example `driverOptions.pool.someProp = '${SOME_MARIADB_CONSTANT}'` will be interpolated as `pool.someProp = mariadb.SOME_MARIADB_CONSTANT`.
- */
-
-/**
- * MariaDB + MySQL specific extension of the {@link SQLERExecOptions} from the [`sqler`](https://ugate.github.io/sqler/) module. When a property of `binds`
- * contains an object it will be _interpolated_ for property values on the `mariadb` module.
- * For example, `binds.name = '${SOME_MARIADB_CONSTANT}'` will be interpolated as
- * `binds.name = mariadb.SOME_MARIADB_CONSTANT`.
- * @typedef {SQLERExecOptions} MDBExecOptions
- * @property {Object} [driverOptions] The `mariadb` module specific options.
- * @property {String} [driverOptions.preparedStatementDatabase] The database name to use when generating prepared statements for the given execution. Since prepared
- * statements are scoped only for a given connection and a temporary stored procedure is used to execute prepared statements, __`preparedStatementDatabase` is
- * required when `execOpts.prepareStatement = true`.__
- * @property {Object} [driverOptions.exec] The options passed into execution/query functions provided by the `mariadb` module performed during {@link Manager.exec}.
- * When a value is a string surrounded by `${}`, it will be assumed to be a _constant_ property that resides on the `mariadb` module and will be interpolated
- * accordingly.
- * For example `driverOptions.exec.someDriverProp = '${SOME_MARIADB_CONSTANT}'` will be interpolated as
- * `driverOptions.exec.someDriverProp = mariadb.SOME_MARIADB_CONSTANT`.
- * @property {Boolean} [driverOptions.exec.namedPlaceholders=true] Truthy to use named parameters in MariaDB/MySQL or falsy to convert the named parameters into a
- * positional array of bind values.
- */
-
-/**
  * MariaDB + MySQL {@link Dialect} implementation for [`sqler`](https://ugate.github.io/sqler/)
  */
 module.exports = class MDBDialect {
@@ -64,8 +22,10 @@ module.exports = class MDBDialect {
     const dlt = internal(this);
     dlt.at.track = track;
     dlt.at.driver = require('mariadb');
-    dlt.at.connections = new Map();
+    dlt.at.transactions = new Map();
+    dlt.at.stmtFuncs = new Map();
     dlt.at.stmts = new Map();
+    dlt.at.connections = new Map();
     dlt.at.opts = {
       autoCommit: true, // default autoCommit = true to conform to sqler
       id: `sqlerMDBGen${Math.floor(Math.random() * 10000)}`,
@@ -147,16 +107,35 @@ module.exports = class MDBDialect {
   /**
    * Begins a transaction by opening a connection from the pool
    * @param {String} txId The transaction ID that will be started
+   * @returns {SQLERTransaction} The transaction
    */
   async beginTransaction(txId) {
     const dlt = internal(this);
-    if (dlt.at.connections.has(txId)) return;
+    if (dlt.at.transactions.has(txId)) {
+      return dlt.at.transactions.get(txId).tx;
+    }
     if (dlt.at.logger) {
       dlt.at.logger(`sqler-mdb: Beginning transaction "${txId}" on connection pool "${dlt.at.opts.id}"`);
     }
-    const conn = await dlt.this.getConnection({ transactionId: txId });
+    const tx = {
+      id: txId,
+      state: Object.seal({
+        isCommitted: false,
+        isRolledback: false,
+        pending: 0
+      })
+    };
+    /** @type {MDBTransactionObject} */
+    const txo = { tx };
+    const opts = { transaction: tx };
+    const conn = await dlt.this.getConnection(opts);
     await conn.beginTransaction();
+    tx.commit = operation(dlt, 'commit', true, conn, opts, 'unprepare'),
+    tx.rollback = operation(dlt, 'rollback', true, conn, opts, 'unprepare'),
+    Object.freeze(tx);
+    dlt.at.transactions.set(txId, txo);
     dlt.at.connections.set(txId, conn);
+    return tx;
   }
 
   /**
@@ -170,6 +149,8 @@ module.exports = class MDBDialect {
    */
   async exec(sql, opts, frags, meta, errorOpts) {
     const dlt = internal(this);
+    /** @type {MDBTransactionObject} */
+    const txo = opts.transaction ? dlt.at.transactions.get(opts.transaction.id) : null;
     let conn, bndp = {}, dopts, rslts, esql, ebndp, error, pso;
     try {
       // interpolate and remove unused binds since
@@ -210,11 +191,12 @@ module.exports = class MDBDialect {
 
       const rtn = {};
 
-      if (!opts.transactionId && !opts.prepareStatement && opts.type === 'READ') {
+      if (!txo && !opts.prepareStatement && opts.type === 'READ') {
         rslts = await dlt.at.pool.query(dopts.exec || esql, ebndp || bndp);
       } else {
         if (opts.prepareStatement) {
           rtn.unprepare = async () => {
+            const hasTX = opts.transaction && dlt.at.transactions.has(opts.transaction.id);
             if (dlt.at.stmts.has(psname)) {
               const pso = dlt.at.stmts.get(psname);
               try {
@@ -223,9 +205,9 @@ module.exports = class MDBDialect {
                 // need to drop separately since drop procedure cannot be done from within a procedure
                 await pso.conn.query(`DROP PROCEDURE ${pso.procedure}`);
               } finally {
-                if (!opts.transactionId) await pso.conn.release();
+                if (!hasTX) await pso.conn.release();
               }
-            } else if (!opts.transactionId) await conn.release();
+            } else if (!hasTX) await conn.release();
           };
           if (isPrepare) {
             pso.name = psname;
@@ -250,10 +232,14 @@ module.exports = class MDBDialect {
           conn = await dlt.this.getConnection(opts);
           rslts = await conn.query(dopts.exec || esql, ebndp || bndp);
         }
-        if (opts.transactionId) {
+        if (txo) {
+          if (rtn.unprepare) {
+            txo.unprepares = txo.unprepares || [];
+            txo.unprepares.push(rtn.unprepare); // keep track of the prepared statements that have transaction scope
+          }
           if (opts.autoCommit) {
             // MariaDB/MySQL has no option to autocommit during SQL execution
-            await operation(dlt, 'commit', false, conn, opts, rtn.unprepare)();
+            await operation(dlt, 'commit', false, conn, opts, 'unprepare')();
           } else {
             dlt.at.state.pending++;
             rtn.commit = operation(dlt, 'commit', true, conn, opts, rtn.unprepare);
@@ -274,7 +260,7 @@ module.exports = class MDBDialect {
       throw err;
     } finally {
       // transactions/prepared statements need the connection to remain open until commit/rollback/unprepare
-      if (conn && !opts.transactionId && !opts.prepareStatement) {
+      if (conn && !txo && !opts.prepareStatement) {
         try {
           await operation(dlt, 'release', false, conn, opts)();
         } catch (cerr) {
@@ -285,14 +271,14 @@ module.exports = class MDBDialect {
   }
 
   /**
-   * Gets the currently open connection or a new connection when no transaction is in progress
+   * Gets the currently open connection or a new connection when there is no transaction already in progress
    * @protected
    * @param {MDBExecOptions} opts The execution options
    * @returns {Object} The connection (when present)
    */
   async getConnection(opts) {
     const dlt = internal(this);
-    const txId = opts.transactionId;
+    const txId = opts.transaction && opts.transaction.id;
     let conn = txId ? dlt.at.connections.get(txId) : null;
     if (!conn) {
       return dlt.at.pool.getConnection();
@@ -312,6 +298,8 @@ module.exports = class MDBDialect {
       }
       if (dlt.at.pool) {
         await dlt.at.pool.end();
+        dlt.at.transactions.clear();
+        dlt.at.stmts.clear();
         dlt.at.connections.clear();
       }
       return dlt.at.state.pending;
@@ -347,20 +335,35 @@ module.exports = class MDBDialect {
  * @param {Boolean} reset Truthy to reset the pending connection and transaction count when the operation completes successfully
  * @param {Object} conn The connection
  * @param {SQLERExecOptions} [opts] The {@link SQLERExecOptions}
- * @param {Function} [preop] A no-argument async function that will be executed prior to the operation
+ * @param {(String | Function)} [preop] A no-argument async function that will be executed prior to the operation or a string that idicates an operation to
+ * perform special actions. The following string operations are valid:
+ * 1. __`unprepare`__ - Any prepared statemsnts that are associated with the `opts.transaction.id` will execute `await preparedStatement.unprepare()`.
  * @returns {Function} A no-arguement `async` function that returns the number or pending transactions
  */
 function operation(dlt, name, reset, conn, opts, preop) {
   return async () => {
+    const txo = opts && opts.transaction ? dlt.at.transactions.get(opts.transaction.id) : null;
     let error;
-    if (preop) await preop();
+    if (preop === 'unprepare') {
+      if (txo.unprepares) {
+        for (let unprepare of txo.unprepares) {
+          await unprepare();
+        }
+        txo.unprepares = null;
+      }
+    } else if (preop) {
+      await preop();
+    }
     try {
       if (dlt.at.logger) {
         dlt.at.logger(`sqler-mdb: Performing ${name} on connection pool "${dlt.at.opts.id}" (${statusLabel(dlt)})`);
       }
       await conn[name]();
       if (reset) { // not to be confused with mariadb connection.reset();
-        if (opts && opts.transactionId) dlt.at.connections.delete(opts.transactionId);
+        if (txo) {
+          dlt.at.transactions.delete(txo.tx.id);
+          dlt.at.connections.delete(txo.tx.id);
+        }
         dlt.at.state.pending = 0;
       }
     } catch (err) {
@@ -448,3 +451,52 @@ let internal = function(object) {
     this: object
   };
 };
+
+/**
+ * MariaDB + MySQL specific extension of the {@link SQLERConnectionOptions} from the [`sqler`](https://ugate.github.io/sqler/) module.
+ * @typedef {SQLERConnectionOptions} MDBConnectionOptions
+ * @property {Object} driverOptions The `mariadb` (w/MySQL support) module specific options. __Both `connection` and `pool` will be merged when generating
+ * the connection pool.__
+ * @property {Object} [driverOptions.connection] An object that will contain properties/values that will be used to construct the MariaDB + MySQL connection
+ * (e.g. `{ database: 'mydb', timezone: '-0700' }`). See the `mariadb` module documentation for a full listing of available connection options.
+ * When a property value is a string surrounded by `${}`, it will be assumed to be a property that resides on either the {@link SQLERPrivateOptions}
+ * passed into the {@link Manager} constructor or a property on the {@link MDBConnectionOptions} itself (in that order of precedence). For example, 
+ * `connOpts.host = '127.0.0.1'` and `driverOptions.connection.host = '${host}'` would be interpolated into `driverOptions.connection.host = '127.0.0.1'`.
+ * In contrast to `privOpts.username = 'someUsername' and `driverOptions.connection.user = '${username}'` would be interpolated into
+ * `driverOptions.connection.user = 'someUsername'`.
+ * Interpoaltions can also contain more than one reference. For example, `driverOptions.connection.someProp = '${protocol}:${host}'` for 
+ * `privOpts = { protocol: 'TCP', host: 'example.com' }` would become `someProp='TCP:example.com'`.
+ * @property {Object} [driverOptions.pool] The pool `conf` options that will be passed into `mariadb.createPool(conf)`. See the `mariadb` module for a full
+ * listing of avialable connection pooling options.
+ * __Using any of the generic `pool.someOption` will override the `conf` options set on `driverOptions.pool`__ (e.g. `pool.max = 10` would override 
+ * `driverOptions.pool.connectionLimit = 20`).
+ * When a value is a string surrounded by `${}`, it will be assumed to be a _constant_ property that resides on the `mariadb` module and will be interpolated
+ * accordingly.
+ * For example `driverOptions.pool.someProp = '${SOME_MARIADB_CONSTANT}'` will be interpolated as `pool.someProp = mariadb.SOME_MARIADB_CONSTANT`.
+ */
+
+/**
+ * MariaDB + MySQL specific extension of the {@link SQLERExecOptions} from the [`sqler`](https://ugate.github.io/sqler/) module. When a property of `binds`
+ * contains an object it will be _interpolated_ for property values on the `mariadb` module.
+ * For example, `binds.name = '${SOME_MARIADB_CONSTANT}'` will be interpolated as
+ * `binds.name = mariadb.SOME_MARIADB_CONSTANT`.
+ * @typedef {SQLERExecOptions} MDBExecOptions
+ * @property {Object} [driverOptions] The `mariadb` module specific options.
+ * @property {String} [driverOptions.preparedStatementDatabase] The database name to use when generating prepared statements for the given execution. Since prepared
+ * statements are scoped only for a given connection and a temporary stored procedure is used to execute prepared statements, __`preparedStatementDatabase` is
+ * required when `execOpts.prepareStatement = true`.__
+ * @property {Object} [driverOptions.exec] The options passed into execution/query functions provided by the `mariadb` module performed during {@link Manager.exec}.
+ * When a value is a string surrounded by `${}`, it will be assumed to be a _constant_ property that resides on the `mariadb` module and will be interpolated
+ * accordingly.
+ * For example `driverOptions.exec.someDriverProp = '${SOME_MARIADB_CONSTANT}'` will be interpolated as
+ * `driverOptions.exec.someDriverProp = mariadb.SOME_MARIADB_CONSTANT`.
+ * @property {Boolean} [driverOptions.exec.namedPlaceholders=true] Truthy to use named parameters in MariaDB/MySQL or falsy to convert the named parameters into a
+ * positional array of bind values.
+ */
+
+/**
+ * Transactions are wrapped in a parent transaction object so private properties can be added (e.g. prepared statements)
+ * @typedef {Object} MDBTransactionObject
+ * @property {SQLERTransaction} tx The transaction
+ * @property {Function[]} unprepares No-argument _async_ functions that will be called as a pre-operation call prior to `commit` or `rollback`
+ */
