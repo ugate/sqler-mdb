357cdb01fb53209f532c0f9854b1c0c6e9f25eb7
ugate1 <ugate.relay@gmail.com>
2026-04-06 12:53:43 -0700

    docs fixes 4x

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -25,20 +25,18 @@ export default defineConfig({
    { text: 'npm', link: 'https://www.npmjs.com/package/sqler-mdb' }
   ],
   sidebar: [
-   {
-    text: 'Guide',
-    items: [
-     { text: 'Overview', link: '/' },
-     { text: 'Getting Started', link: '/guide/getting-started' },
-     { text: 'Manual', link: '/guide/manual' }
-    ]
-   },
-   {
-    text: 'API',
-    items: [
-     { text: 'API Reference', link: '/api/' }
-    ]
-   }
+    {
+      text: 'Guide',
+      items: [
+        { text: 'Overview', link: '/' },
+        { text: 'Getting Started', link: '/guide/getting-started' },
+        { text: 'Manual', link: '/guide/manual' }
+      ]
+    },
+    {
+      text: 'API',
+      items: apiSidebar[0].items
+    }
   ],
   socialLinks: [
    { icon: 'github', link: 'https://github.com/ugate/sqler-mdb' }
